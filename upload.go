326823197c881b69bd18eac58fbfe9e4a7c452eb
Tad DeVries <tadd@goldenwest.com>
2015-12-10 14:17:58 -0700

    version 0.0.2

--- upload.go
+++ upload.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/base64"
-	"fmt"
-	"io/ioutil"
-	"io"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
-	"strings"
-	//"crypto/sha1"
 	"os"
+	"strconv"
+	"strings"
+	//"bytes"
+	"encoding/hex"
 )
 
 type Account struct {
@@ -22,9 +25,9 @@ type Bucket struct {
 }
 
 type Error struct {
-	Code	string
-	Message	string
-	Status	int
+	Code    string
+	Message string
+	Status  int
 }
 
 type Session struct {
@@ -41,16 +44,29 @@ type Upload struct {
 }
 
 type UploadFile struct {
+	fileName string
 	filePath string
 	fileType string
 	fileSha1 string
+	fileSize string
+}
+
+type UploadResponse struct {
+	FileId        string
+	FileName      string
+	AccountId     string
+	BucketId      string
+	ContentLength int
+	ContentSha1   string
+	ContentType   string
+	FileInfo      interface{}
 }
 
 func readJSON(r interface{}, v interface{}) error {
 	var parsee interface{}
 	var err error
 
-	switch r.(type){
+	switch r.(type) {
 	case string:
 		file, err := os.Open(r.(string))
 		if err != nil {
@@ -59,7 +75,7 @@ func readJSON(r interface{}, v interface{}) error {
 		defer file.Close()
 		parsee = file
 	case io.Reader:
-		parsee = r.(io.Reader)
+		parsee = r
 	}
 
 	err = json.NewDecoder(parsee.(io.Reader)).Decode(&v)
@@ -73,17 +89,17 @@ func readJSON(r interface{}, v interface{}) error {
 func apiRequest(method string, headers map[string]string, url string, v interface{}, body string) error {
 	// setup the request
 	req, err := http.NewRequest(method, url, strings.NewReader(body))
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
 	// set headers from map
-	for k, m := range headers{
+	for k, m := range headers {
 		fmt.Println("Adding Header: ", k, " => ", m)
-		req.Header.Set(k, m)	
+		req.Header.Set(k, m)
 	}
 
-	fmt.Println("Opening: ", url)
+	fmt.Println("Requesting: ", url)
 
 	// send the request
 	client := &http.Client{}
@@ -94,7 +110,7 @@ func apiRequest(method string, headers map[string]string, url string, v interfac
 	defer res.Body.Close()
 
 	// return parsed information on success, or output error message on failure
-	switch res.StatusCode{
+	switch res.StatusCode {
 	case 200:
 		fmt.Println("Status: ", res.Status)
 		// parse the response
@@ -115,17 +131,19 @@ func apiRequest(method string, headers map[string]string, url string, v interfac
 	return nil
 }
 
-
 func main() {
 	authURL := "https://api.backblaze.com"
 	apiPath := "/b2api/v1"
 	headers := make(map[string]string)
-	//apiUpload := "/b2_upload_file"
+
+	///////////////////////////////////
+	// GET Initial Authorization Token
+	///////////////////////////////////
 
 	// read in the account information
 	var account Account
 	err := readJSON("account.json", &account)
-	if err != nil{
+	if err != nil {
 		fmt.Println("error: ", err)
 		return
 	}
@@ -137,12 +155,17 @@ func main() {
 	// get authorized
 	var session Session
 	err = apiRequest("GET", headers, authURL+apiPath+"/b2_authorize_account", &session, "")
-	if err != nil{
+	if err != nil {
 		fmt.Println("error: ", err)
 	}
 
 	fmt.Printf("%+v", session)
 	fmt.Println()
+	fmt.Println("---")
+
+	///////////////////
+	// POST Upload URL
+	///////////////////
 
 	// put the authorization token in the header
 	headers["Authorization"] = session.AuthorizationToken
@@ -159,5 +182,50 @@ func main() {
 
 	fmt.Printf("%+v", upload)
 	fmt.Println()
+	fmt.Println("---")
 
+	////////////////////
+	// POST Upload File
+	////////////////////
+
+	uploadFile := UploadFile{fileName: "background.png", filePath: "background.png", fileType: "image/png", fileSha1: "", fileSize: ""}
+
+	// open upload file
+	file, err := os.Open(uploadFile.filePath)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	defer file.Close()
+
+	//get file size
+	fileInfo, _ := file.Stat()
+	uploadFile.fileSize = strconv.FormatInt(fileInfo.Size(), 10)
+
+	//get sha1 of file contents
+	fileContent, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	hash := sha1.New()
+	hash.Write(fileContent)
+	uploadFile.fileSha1 = hex.EncodeToString(hash.Sum(nil))
+
+	// build the headers
+	headers["Authorization"] = upload.AuthorizationToken
+	headers["X-Bz-File-Name"] = uploadFile.filePath
+	headers["Content-Type"] = uploadFile.fileType
+	headers["Content-Length"] = uploadFile.fileSize
+	headers["X-Bz-Content-Sha1"] = uploadFile.fileSha1
+
+	fmt.Printf("%+v", headers)
+	fmt.Println()
+
+	var uploadResponse UploadResponse
+	err = apiRequest("POST", headers, upload.UploadUrl, &uploadResponse, string(fileContent))
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	fmt.Printf("%+v", uploadResponse)
+	fmt.Println()
 }
